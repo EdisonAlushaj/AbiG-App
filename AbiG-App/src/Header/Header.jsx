5614b2864acd930833c12ae33e34f1f11582b917
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2024-08-19 23:06:56 +0200

    App Update
    Shtimi i Logos dher i Fonts Family

--- AbiG-App/src/Header/Header.jsx
+++ AbiG-App/src/Header/Header.jsx
@@ -1,23 +1,23 @@
 import { useEffect } from "react";
 import AppRoutes from "../AppRoutes";
 import { NavLink } from "react-router-dom";
+import Logo from '../assets/Logo-transparent.png'
 
 function Header() {
 
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-center" style={{ width: '100%', height: '100px', zIndex: '2', position: 'absolute' }}>
-                <div className="container-fluid d-flex justify-content-center" style={{ width: '100%' }}>
-                    <a className="navbar-brand" style={{ color: 'black' }} href="#">Logo Abi-G</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                        <ul className="navbar-nav gap-5 align-items-center" style={{fontSize: '1.2em'}}>
-                            <li className="nav-item" ><NavLink to="/app/home" style={{ color: 'black', textDecoration: 'none' }}>Home</NavLink></li>
-                            <li className="nav-item"><NavLink to="/app/about" style={{ color: 'black', textDecoration: 'none' }}>About Us</NavLink></li>
-                            <li className="nav-item"><NavLink to="#" style={{ color: 'black', textDecoration: 'none' }}>Services</NavLink></li>
-                            <li className="nav-item"><NavLink to="/app/contact" style={{ color: 'black', textDecoration: 'none' }}>Contact Us</NavLink></li>
+                <div className="container-fluid d-flex" style={{ width: '75%', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                    <NavLink className="navbar-brand" to="/app/home" style={{  }}>
+                        <img src={Logo} alt="Logo" style={{width: '3.5em', height: '3.5em'}} />
+                    </NavLink>
+                    <div className="collapse navbar-collapse justify-content-end" style={{marginRight: ''}}>
+                        <ul className="navbar-nav gap-5 align-items-center" style={{fontSize: '1.4em'}}>
+                            <li className="nav-item" ><NavLink to="/app/home" style={{ color: '#19282F', textDecoration: 'none' }}><b>Home</b></NavLink></li>
+                            <li className="nav-item"><NavLink to="/app/about" style={{ color: '#19282F', textDecoration: 'none' }}><b>About Us</b></NavLink></li>
+                            <li className="nav-item"><NavLink to="#" style={{ color: '#19282F', textDecoration: 'none' }}><b>Services</b></NavLink></li>
+                            <li className="nav-item"><NavLink to="/app/contact" style={{ color: '#19282F', textDecoration: 'none' }}><b>Contact Us</b></NavLink></li>
                         </ul>
                     </div>
                 </div>
